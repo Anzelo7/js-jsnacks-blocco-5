ad5c8f36f226ea91e7b085812a1357018d6f3436
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-09 16:50:29 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,71 @@
+console.log('JS OK')
+
+/* SNACK 1*/
+/*
+ * Creare un array di oggetti di squadre di calcio. 
+ * Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+ * Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+ * Generare numeri random al posto degli 0 nelle proprietà:
+ * punti fatti e falli subiti
+*/
+const teams = [
+    {
+        nome: "Juventus",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Benevento",
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+
+]
+
+teams.forEach((element) => {
+    const { puntiFatti, falliSubiti } = element;
+    element.puntiFatti = getRandomNumber(1, 5);
+    element.falliSubiti = getRandomNumber(1, 5);
+
+    console.log(element);
+});
+
+
+/* SNACK 2*/
+/*
+ * Si scriva una funzione che accetti tre argomenti, 
+ * un array e due numeri (a più piccolo di b). 
+ * La funzione ritornerà un nuovo array con i valori 
+ * che hanno la posizione compresa tra i due numeri.
+ */
+
+function createNewArray(arr, num1, num2) {
+    let arrayAppoggio = [];
+
+    if (num1 < num2) {
+        arr.forEach((number) => {
+            if (number > num1 && number < num2) {
+                arrayAppoggio.push(number);
+            }
+        })
+        return arrayAppoggio;
+    }
+}
+
+const array = [2, 5, 8, 5, 4, 9,];
+createNewArray(array, 1, 20);
+console.log(createNewArray(array, 1, 20));
+
+
+/* SNACK 3*/
+/* 
+ * Crea 10 oggetti che rappresentano una zucchina.
+ * Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+ * Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+ */
+
